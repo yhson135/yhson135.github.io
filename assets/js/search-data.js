7d34d2d2dbccb8ce20362a2826c84d23574d909d
yhson135 <42964811+yhson135@users.noreply.github.com>
2025-06-22 09:41:13 +0000

    Deploying to gh-pages from @ yhson135/yhson135.github.io@2f550b5f04dc0457c112fb150a756e20b3395dae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,11 +17,11 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/CV/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
